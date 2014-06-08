6409700de8f78eb38a177022b51ee6868a782ede
shendyaprianto <shendyaprianto@users.noreply.github.com>
2014-06-09 06:07:54 +0700

    Update ChatServerAdaptor.java

--- src/chatapp/ChatServerAdaptor.java
+++ src/chatapp/ChatServerAdaptor.java
@@ -27,7 +27,7 @@ public static List<IUser> users = new ArrayList<IUser>();
     public static void removeUser(IUser user)
     {
  	zone.removeUser(user);
- 	users.remove(user);
+ //	users.remove(user);
    	  System.out.println("User Removed : "+user.getName());
     
     }
